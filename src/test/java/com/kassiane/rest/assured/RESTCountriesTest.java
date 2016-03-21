c1652bb6d46b7efa7f9089cec50442d358a21ddb
Kassiane <kassiane@gmail.com>
2016-03-21 00:47:01 -0300

    First example using jsonPath.

--- src/test/java/com/kassiane/rest/assured/RESTCountriesTest.java
+++ src/test/java/com/kassiane/rest/assured/RESTCountriesTest.java
@@ -1,13 +1,18 @@
 package com.kassiane.rest.assured;
 
-import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
 
 import org.junit.Test;
 
 public class RESTCountriesTest {
 
     @Test
-    public void testGetAllCountries() {
-        get("http://restcountries.eu/rest/v1/all");
+    public void getAllCountriesSuccessfully() {
+        final List<String> countryNames = when().get("http://restcountries.eu/rest/v1/all").jsonPath().getList("name");
+        assertThat(countryNames.size(), greaterThanOrEqualTo(247));
     }
 }
